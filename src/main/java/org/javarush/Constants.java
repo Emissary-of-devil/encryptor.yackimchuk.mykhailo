9875069942919477412ccfbd8f93e23e7544f60c
Yackimchuk Mykhailo <yackimchuk@gmail.com>
2025-03-17 15:58:28 +0200

    Fixing mistakes

--- src/main/java/org/javarush/Constants.java
+++ src/main/java/org/javarush/Constants.java
@@ -2,9 +2,10 @@ package org.javarush;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class Constants {
-    static final ArrayList<Character> Alphabet = new ArrayList<>(Arrays.asList
+    static final List<Character> Alphabet = new ArrayList<>(Arrays.asList
             ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
